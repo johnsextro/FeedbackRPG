0b877d9f55e7a40340a94d0e68da5ecb1ce5d106
John Sextro <john.sextro@asynchrony.com>
2019-03-02 19:19:00 -0600

    wired up an action that randomizes a persona for the responder

--- App/Containers/ResponderScreen.js
+++ App/Containers/ResponderScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, View, Image } from 'react-native'
 import { connect } from 'react-redux'
+import RoundedButton from '../Components/RoundedButton'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 import { Images } from '../Themes'
@@ -8,10 +9,22 @@ import { Images } from '../Themes'
 import styles from './Styles/ResponderScreenStyle'
 
 class ResponderScreen extends Component {
-  // constructor (props) {
-  //   super(props)
-  //   this.state = {}
-  // }
+  constructor (props) {
+    super(props)
+    this.randomizePersona = this.randomizePersona.bind(this)
+    this.state = { persona: ''};
+  }
+
+  randomizePersona() {
+    var personas = ['Bard', 'Paladin', 'Mage', 'Rogue', 'Wizard', 'Monk']
+    for (var i = personas.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = personas[i];
+      personas[i] = personas[j];
+      personas[j] = temp;
+    }
+    this.setState({ persona: personas[0] });
+  }
 
   render () {
     const {navigate} = this.props.navigation;
@@ -24,14 +37,18 @@ class ResponderScreen extends Component {
               Playing as the Responder
             </Text>
           </View>
+          <RoundedButton
+              onPress={this.randomizePersona}
+              text='Randomize My Persona'
+            />
           <View style={styles.instruction} >
             <Text style={styles.titleText}>
-              Persona
+              { this.state.persona }
             </Text>
           </View>
           <View style={styles.instruction} >
             <Text style={styles.titleText}>
-              Instructions for playing as thi persona.
+              Instructions for playing as this persona.
             </Text>
           </View>
         </ScrollView>
