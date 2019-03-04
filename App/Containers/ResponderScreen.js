946c143bc4ddf1afb891b10bd0c8503be2445955
John Sextro <john.sextro@asynchrony.com>
2019-03-04 13:36:38 -0600

    added the motivations for the personas.

--- App/Containers/ResponderScreen.js
+++ App/Containers/ResponderScreen.js
@@ -16,7 +16,19 @@ class ResponderScreen extends Component {
   }
 
   randomizePersona() {
-    var personas = ['Bard', 'Paladin', 'Mage', 'Rogue', 'Wizard', 'Monk']
+    var personas = [{role: 'Bard', 
+                    motivation: 'You use long drawn out explanations and answers in an effort to dominate the conversation. You weave tales from past projects (battles) to distract away from the core topic. Your goal is to run out the clock to avoid ever hearing real feedback.'},
+                    {role: 'Paladin',
+                    motivation: 'You are the master of deflection, but you would never throw a co-worker under the bus. You always find a way to avoid accepting personal responsibility by down playing the impact of the issue or pointing out the silver lining. Your goal is to gain agreement that things are fine as-is.'}, 
+                    {role: 'Berserker',
+                    motivation: 'You are angry and extremely disagreeable when offered feedback and coaching. You will never agree with any facts presented to you. Your behavior is very close to the border line of what would commonly be considered work inappropriate, regularly straying into unprofessional. Your goal is to frighten away coaching.'}, 
+                    {role: 'Rogue',
+                    motivation: 'You use slight of hand and misdirection to slither out of every tough situation. You always have pocket sand ready to throw in someone’s eyes. You will never accept personal responsibility for anything that isn’t successful. Your goal is to avoid agreeing with anything you hear.'}, 
+                    {role: 'Wizard',
+                    motivation: 'You are wise, maybe a little too wise. You know and are willing to accept responsibility or at least what on the surface looks like responsibility. You try to jump ahead and show how much you’ve already improved. Your goal is to quickly gain agreement that things are already better by demonstrating how smart you are and how much you’ve “already improved”.'},
+                    {role: 'Monk',
+                    motivation: 'You have attained a level of enlightenment. You want to actively work to find the truth in what you are hearing. You easily accept personal responsibility for your actions. Feedback is as valuable to you as the rarest gem. Your goal is to improve and find a way to demonstrate and measure your improvement over time.'}
+                  ]
     for (var i = personas.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
       var temp = personas[i];
@@ -43,12 +55,10 @@ class ResponderScreen extends Component {
             />
           <View style={styles.instruction} >
             <Text style={styles.titleText}>
-              { this.state.persona }
+              { this.state.persona.role }
             </Text>
-          </View>
-          <View style={styles.instruction} >
             <Text style={styles.titleText}>
-              Instructions for playing as this persona.
+              { this.state.persona.motivation }
             </Text>
           </View>
         </ScrollView>
